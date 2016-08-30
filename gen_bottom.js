d2f2e922f7ab64800fc2f2d48f54e22c68179478
Utkin Artem <solid.utkin@gmail.com>
2016-08-30 18:28:36 +0300

    add incline bottom and some fixs

--- gen_bottom.js
+++ gen_bottom.js
@@ -6,7 +6,7 @@ function Gen_bottom_func() {
   this.width = 100;
   this.height = 100;
   this.deep = 10;
-
+  
   function matrixArray(rows, colomns) {
     var arr = [];
     for(var i = 0; i < colomns; i++) {
@@ -15,21 +15,31 @@ function Gen_bottom_func() {
         arr[i][j] = 0;
       }
     }
+    return arr;
   }
 
   //генерация плоского дна
-  function flat_bottom() {
-  	var matrix_of_coordinates = matrixArray(this.width, this.height);
+  this.get_flat_bottom = function(x, y) {
+  	var matrix_of_coordinates = matrixArray(this.width + 1, this.height + 1);
 
     for(var i = 0; i <= this.width; i++) {
       for(var j = 0; j <= this.height; j++) {
         matrix_of_coordinates[i][j] = this.deep;
       }
     }
+    return matrix_of_coordinates[x][y]; 
   }
-
-  this.get_flat_bottom = function(x, y) {
-  	flat_bottom();
-    return matrix_of_coordinates[i][j];
+  
+  //генерация наклонного дна
+  this.get_inclined_bottom = function(x, y, alpha) {
+  	var matrix_of_coordinates = matrixArray(this.width + 1, this.height + 1);
+  	
+  	for(var i = 0; i <= this.width; i++) {
+      for(var j = 0; j <= this.height; j++) {
+        matrix_of_coordinates[i][j] = this.deep + Math.tan(alpha * Math.PI / 180) * j;
+      }
+    }
+    return matrix_of_coordinates[x][y];
   }
 }
+
