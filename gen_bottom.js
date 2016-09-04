d7264c372acbc02c952e17e945b8614f8905ee00
Utkin Artem <solid.utkin@gmail.com>
2016-09-04 21:47:51 +0300

    add interpolation

--- gen_bottom.js
+++ gen_bottom.js
@@ -29,7 +29,7 @@ function Gen_bottom_func() {
     }
     return matrix_of_coordinates[x][y]; 
   }
-  
+      
   //генерация наклонного дна
   this.get_inclined_bottom = function(x, y, alpha) {
   	var matrix_of_coordinates = matrixArray(this.width + 1, this.height + 1);
@@ -52,5 +52,61 @@ function Gen_bottom_func() {
       }
     }
     return matrix_of_coordinates[x][y];
+  }
+}
+
+// Generation bottom with using income data
+// Библиотека генерирующая дно, использющая входящие данные 
+
+function Gen_bottom_inter() {
+
+  this.width = 100;
+  this.height = 100;
+
+  function matrixArray(rows, colomns) {
+    var arr = [];
+    for(var i = 0; i < colomns; i++) {
+      arr[i] = [];
+      for(var j = 0; j < rows; j++) {
+        arr[i][j] = 0;
+      }
+    }
+    return arr;
+  }
+
+  //интерполирующая функция (билинейная интерполяция)
+  this.interpolate = function(_x, _y, dataArray) {
+    var matrix_of_coordinates = matrixArray(this.width + 1, this.height + 1);
+
+    var x1_ind = 0;
+    var x2_ind = 1;
+    var y1_ind = 0;
+    var y2_ind = 1;
+
+    var x1=0, x2=0, y1=0, y2=0;
+
+    for(var x = 0; x <= this.width; x++){
+      if(x >= x2 && x2 !== 0 && x2 !== this.width) {
+        x1_ind++;
+        x2_ind++;
+      }
+      for(var y = 0; y <= this.height; y++) {
+        if(y >= y2 && y2 !== 0 && y2 !== this.height) {
+          y1_ind++;
+          y2_ind++;
+        }
+
+        x1 = dataArray[x1_ind][0][0];
+        x2 = dataArray[x2_ind][0][0];
+        y1 = dataArray[0][y1_ind][1];
+        y2 = dataArray[0][y2_ind][1];
+
+        var f_q11 = dataArray[x1_ind][y1_ind][2], f_q12 = dataArray[x1_ind][y2_ind][2],
+        f_q21 = dataArray[x2_ind][y1_ind][2], f_q22 = dataArray[x2_ind][y2_ind][2];
+
+                matrix_of_coordinates[x][y] = f_q11*(x2-x)*(y2-y)/((x2-x1)*(y2-y1)) + f_q21*(x-x1)*(y2-y)/((x2-x1)*(y2-y1)) + f_q12*(x2-x)*(y-y1)/((x2-x1)*(y2-y1)) + f_q22*(x-x1)*(y-y1)/((x2-x1)*(y2-y1));
+      }
+    }
+    return matrix_of_coordinates[_x][_y];
   }
 }
